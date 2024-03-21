50d1e4e016c514334fa4c26ccc877d015a76af99
Bells307 <sdikansky96@gmail.com>
2024-03-21 09:26:47 +0300

    add readme

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,21 @@ mod tp;
 pub use crate::rt::handle::JoinHandle;
 
 use futures::Future;
+use reactor::Reactor;
 use rt::{builder::AsyncRuntimeBuilder, AsyncRuntime};
 use std::sync::{
     atomic::{AtomicBool, Ordering},
     OnceLock,
 };
 
-static TERMINATED: AtomicBool = AtomicBool::new(false);
+/// Global runtime
 static RUNTIME: OnceLock<AsyncRuntime> = OnceLock::new();
+
+/// Runtime terminated flag
+static TERMINATED: AtomicBool = AtomicBool::new(false);
 
 pub fn builder() -> AsyncRuntimeBuilder {
     AsyncRuntimeBuilder::new()
-}
-
-pub fn runtime() -> &'static AsyncRuntime {
-    if TERMINATED.load(Ordering::SeqCst) {
-        panic!("runtime is already terminated");
-    }
-
-    RUNTIME.get().expect("runtime is not set")
 }
 
 pub fn block_on<F>(fut: F)
@@ -45,8 +41,26 @@ where
 impl AsyncRuntime {
     /// Register runtime
     pub fn register(self) {
+        check_terminated();
+
         if RUNTIME.set(self).is_err() {
             panic!("runtime register error");
         }
+    }
+}
+
+pub(crate) fn reactor() -> &'static Reactor {
+    runtime().reactor()
+}
+
+fn runtime() -> &'static AsyncRuntime {
+    check_terminated();
+
+    RUNTIME.get().expect("runtime is not set")
+}
+
+fn check_terminated() {
+    if TERMINATED.load(Ordering::SeqCst) {
+        panic!("runtime is already terminated");
     }
 }
